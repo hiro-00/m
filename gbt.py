764967c6c0f56ab92338f0b24b53ce9941649fa0
hiro-00 <h.fujita.0016@gmail.com>
2017-06-12 00:26:59 -0700

    add gbt and rf

--- gbt.py
+++ gbt.py
@@ -10,7 +10,6 @@ from sklearn.metrics import mean_squared_error, r2_score
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
 
-
 # Remove non-onformative columns
 cols_to_remove = []
 for c in test.columns:
@@ -19,8 +18,6 @@ for c in test.columns:
 print('Columns to remove: ' + str(cols_to_remove))
 train = train.drop(cols_to_remove, axis=1)
 test = test.drop(cols_to_remove, axis=1)
-
-
 
 # Add some categorical features
 train['X0_0'] = train['X0'].apply(lambda x: x[0])
@@ -35,7 +32,6 @@ train['X2_1'] = train['X2'].apply(lambda x: x[1] if len(x) > 1 else 'empty')
 test['X2_0'] = test['X2'].apply(lambda x: x[0])
 test['X2_1'] = test['X2'].apply(lambda x: x[1] if len(x) > 1 else 'empty')
 
-
 # process columns, apply LabelEncoder to categorical features
 for c in train.columns:
     if train[c].dtype == 'object':
@@ -44,7 +40,6 @@ for c in train.columns:
         train[c] = lbl.transform(list(train[c].values))
         test[c] = lbl.transform(list(test[c].values))
 
-
 ##Add decomposed components: PCA / ICA etc.
 from sklearn.decomposition import PCA, FastICA
 from sklearn.decomposition import TruncatedSVD
@@ -52,27 +47,27 @@ from sklearn.decomposition import TruncatedSVD
 n_comp = 12
 
 # tSVD
-tsvd = TruncatedSVD(n_components=n_comp, random_state=400)
+tsvd = TruncatedSVD(n_components=n_comp, random_state=420)
 tsvd_results_train = tsvd.fit_transform(train.drop(["y"], axis=1))
 tsvd_results_test = tsvd.transform(test)
 
 # PCA
-pca = PCA(n_components=n_comp)# random_state=400)
+pca = PCA(n_components=n_comp, random_state=420)
 pca2_results_train = pca.fit_transform(train.drop(["y"], axis=1))
 pca2_results_test = pca.transform(test)
 
 # ICA
-ica = FastICA(n_components=n_comp)#, random_state=400)
+ica = FastICA(n_components=n_comp, random_state=420)
 ica2_results_train = ica.fit_transform(train.drop(["y"], axis=1))
 ica2_results_test = ica.transform(test)
 
 # GRP
-grp = GaussianRandomProjection(n_components=n_comp, eps=0.007, random_state=400)
+grp = GaussianRandomProjection(n_components=n_comp, eps=0.1, random_state=420)
 grp_results_train = grp.fit_transform(train.drop(["y"], axis=1))
 grp_results_test = grp.transform(test)
 
 # SRP
-srp = SparseRandomProjection(n_components=n_comp, dense_output=True, random_state=400)
+srp = SparseRandomProjection(n_components=n_comp, dense_output=True, random_state=420)
 srp_results_train = srp.fit_transform(train.drop(["y"], axis=1))
 srp_results_test = srp.transform(test)
 
@@ -98,58 +93,52 @@ train = train.drop(["y"], axis=1)
 y_mean = np.mean(y_train)
 
 ### Regressor
-import xgboost as xgb
+from sklearn.ensemble import GradientBoostingRegressor
 
 # prepare dict of params for xgboost to run with
-xgb_params = {
-    'n_trees': 520,
-    'eta': 0.0045,
-    'max_depth': 4,
-    'subsample': 0.98,
-    'objective': 'reg:linear',
-    'eval_metric': 'rmse',
-    'base_score': y_mean,  # base prediction = mean(target)
-    'silent': 1
+param = {
+    #"learning_rate":0.001,
+    # "loss" : "huber",
+    # "max_depth": 3,
+    # "max_features" : 0.55,
+    # "min_samples_leaf" : 18,
+    # "min_samples_split" : 14,
+    # "subsample" : 0.7
 }
 
-
-
-num_boost_rounds = 1350
 def cv():
     cv_num = 3
     nfolds = 5
     score = 0
     for _ in range(cv_num):
-        kfold = KFold(n=train.shape[0], n_folds = nfolds,  random_state=_, shuffle=True)
+        kfold = KFold(n=train.shape[0], n_folds=nfolds, random_state=_, shuffle=True)
         for train_index, test_index in kfold:
+            x_train = train.values[train_index]
+            label_train = y_train.values[train_index]
+            x_test = train.values[test_index]
+            label_test =y_train[test_index]
 
-            dtrain = xgb.DMatrix(train.values[train_index], label = y_train.values[train_index])
-            dtest = xgb.DMatrix(train.values[test_index], label = y_train[test_index])
+            rf = GradientBoostingRegressor(**param)
+            rf.fit(x_train, label_train)
+            y_pred = rf.predict(x_test)
 
-            model = xgb.train(dict(xgb_params), dtrain, num_boost_round=num_boost_rounds)
-            y_pred = model.predict(dtest)
-
-            overfit_test = model.predict(dtrain)
-            print("ov:"+str(r2_score(dtrain.get_label(), overfit_test)))
+            overfit_test = rf.predict(x_train)
+            print("ov:" + str(r2_score(label_train, overfit_test)))
             print(r2_score(y_train.values[test_index], y_pred))
-            score += r2_score(dtest.get_label(), y_pred)
+            score += r2_score(label_test, y_pred)
         print("--")
-    print(score / (cv_num * nfolds) )
+    print(score / (cv_num * nfolds))
+
 
 def submit():
-
-    dtrain = xgb.DMatrix(train.values, label = y_train.values)
-    dtest = xgb.DMatrix(test.values)
-    model = xgb.train(dict(xgb_params, silent=1), dtrain, num_boost_round=num_boost_rounds)
-
-
-    # print(r2_score(model.predict(dtrain), dtrain.get_label()))
-
-    # make predictions and save results
-    y_pred = model.predict(dtest)
+    rf = GradientBoostingRegressor(**param)
+    rf.fit( train.values, y_train.values)
+    y_pred = rf.predict(test.values)
 
     output = pd.DataFrame({'id': test['ID'].astype(np.int32), 'y': y_pred})
     output.to_csv('submission_baseLine.csv', index=False)
 
+
 cv()
-submit()
+#submit()
+
